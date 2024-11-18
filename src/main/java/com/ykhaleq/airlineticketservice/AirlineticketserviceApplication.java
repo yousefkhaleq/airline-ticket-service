ffba38aa43a019766f2b1d341b8ec42d504706c7
Yousef Khaleq <56322765+yousefkhaleq@users.noreply.github.com>
2024-11-17 18:10:33 -0800

    Initial commit for airline project

---
+++ src/main/java/com/ykhaleq/airlineticketservice/AirlineticketserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ykhaleq.airlineticketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineticketserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineticketserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ykhaleq/airlineticketservice/AirlineticketserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ykhaleq.airlineticketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineticketserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
